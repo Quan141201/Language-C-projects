5b03f40fae328222bfde9ccf6e93d188391bb585
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-04 22:11:39 -0500

    Add files via upload

---
+++ Baiso009.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h";
+int main () {
+	//Khai bao bien a, b
+	int a, b, min, max;
+	
+	//Nhap, xuat a, b
+	printf ("Nhap vao a = ");
+	scanf ("%d", &a);
+	printf ("\nNhap vao b = ");
+	scanf ("%d", &b);	
+	
+	//Xu ly du lieu 
+	min = (a < b) ? a : b;
+	max = (a > b) ? a : b;
+	
+	//Xuat ket qua ra man hinh
+	printf ("\nGia tri nho nhat la %d", min);
+	printf ("\nGia tri lon nhat la %d", max);
+}
