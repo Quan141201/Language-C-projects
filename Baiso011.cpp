45855cd754d159e8937e2bee8ac1429a4545cc21
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-07 22:05:43 -0500

    Add files via upload

---
+++ Baiso011.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main () {
+	int a, b;
+	printf ("Nhap vao a = ");
+	scanf ("%d", &a);
+	printf ("Nhap vao b = ");
+	scanf ("%d", &b);
+	
+	float ket_qua = (float)a/b;
+	int ket_qua2 = (int)a/b;
+	printf ("%d / %d = %.2f", a, b, ket_qua);
+	printf ("\nKet qua 2 = %d", ket_qua2);
+}
