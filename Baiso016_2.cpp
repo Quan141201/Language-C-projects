f6c6c397f0febadcc79c42818c8ee0338a309090
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-12 01:32:12 -0500

    Create Baiso016_2.cpp

---
+++ Baiso016_2.cpp
@@ -0,0 +1,28 @@
+// Cach thay chua 
+
+#include "stdio.h"
+#include "math.h"
+
+int main () 
+{
+    float a, b, c, delta, x1, x2;
+
+    printf ("Phuong trnh bac 2 ax^2 + bx + c = 0 ");
+    printf ("\nNhap vao he so a, b, c ");
+    scanf ("%f%f%f", &a, &b, &c);
+
+    delta = pow(b,2) - 4*a*c; 
+    printf ("\ndelta = %.2f", delta );
+     
+    if (delta > 0) {
+        x1 = ((-b+sqrt(delta))/(2*a));
+        x2 = ((-b-(sqrt(delta)))/(2*a));
+        printf ("\nx1 = %.2f, \nx2 = %.2f", x1, x2  ); 
+    } else if (delta == 0) {
+        x1 = x2 = (-b/(2*a));
+        printf ("\nx1 = %.2f, \nx2 = %.2f", x1, x2);
+    } else {
+        printf ("\nPhuong trinh vo nghiem");
+    }
+
+}
