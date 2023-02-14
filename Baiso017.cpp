612f5c662dd1eaa88871631bc9fec3498b354fea
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-14 00:20:11 -0500

    Add files via upload

---
+++ Baiso017.cpp
@@ -0,0 +1,33 @@
+#include "stdio.h"
+int main () {
+    int n;
+    printf ("Nhap vao so tu 1 den 7: ");
+    scanf ("%d", &n);
+
+    switch (n) {
+        case 1: 
+            printf ("Chu nhat");
+            break;
+        case 2:
+            printf ("\nThu 2");
+            break;
+        case 3:
+            printf ("\nThu 3");
+            break;
+        case 4:
+            printf ("\nThu 4");
+            break;
+        case 5:
+            printf ("\nThu 5");
+            break;
+        case 6:
+            printf ("\nThu 6");
+            break;
+        case 7:
+            printf ("\nThu 7");
+            break;    
+        
+        default:
+            printf ("So n khong dung. ");
+    }
+}
