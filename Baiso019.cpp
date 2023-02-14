a93d14ec3509e810005cf189e8646975dc4c36c6
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-14 18:41:05 -0500

    Update Baiso019.cpp

--- Baiso019.cpp
+++ Baiso019.cpp
@@ -3,40 +3,44 @@
 
 int main () {
     // Bien
-    float xa, ya, xb, yb, xc, yc;
-    float AB, BC, CA;
-    float chu_vi, dien_tich, p; 
+    float ax ,bx ,cx ,ay ,by ,cy ;
+    float AB, BC, CA, p, chu_vi, dien_tich;
 
-    // Nhap vao toa do 
-    printf ("Nhap vao toa do diem A(xa, ya) = ");
-    scanf ("%f%f", &xa, &ya);
-    printf ("\nNhap vao toa do diem B(xb, yb) = ");
-    scanf ("%f%f", &xb, &yb);
-    printf ("\nNhap vao toa do diem C(xc, yc) = ");
-    scanf ("%f%f", &xc, &yc);
+    // Nhap toa do 3 dinh A, B, C
+    printf ("Nhap toa do diem A(ax,ay) = ");
+    scanf ( "%f%f", &ax, &ay);
+    printf ("Nhap toa do diem B(bx,by) = ");
+    scanf ( "%f%f", &bx, &by);
+    printf ("Nhap toa do diem C(cx,cy) = ");
+    scanf ( "%f%f", &cx, &cy);
 
-    // Tinh khoang cach
-    AB = sqrt(pow(xa-xb, 2) + pow(ya-ya, 2));
-    BC = sqrt(pow(xb-xc, 2) + pow(yb-yc, 2));
-    CA = sqrt(pow(xc-xa, 2) + pow(yc-ya, 2));
+    // Tinh do dai doan thang AB, BC, CA 
+    AB = sqrt((pow((bx-ax),2)) + (pow((by-ay),2))); 
+    printf ("\nAB = %.2f", AB); 
+    BC = sqrt((pow((cx-bx),2)) + (pow((cy-by),2)));
+    printf ("\nBC = %.2f", BC);  
+    CA = sqrt((pow((ax-cx),2)) + (pow((ay-cy),2)));
+    printf ("\nCA = %.2f", CA);  
 
-    // Kiem tra xem co tao thanh tam giac 
-    if (((AB + BC) > CA) && ((AB + CA) > BC) && ((BC + CA) > AB)) {
-        printf ("\nABC khong tao thanh tam giac");
-        //Tam giac can 
-        if ((AB == BC) || (AB == CA)|| (BC == CA)) {
+    // Kiem tra A, B, C co tao thanh tam giac khong
+    if (((AB + BC) > CA) && ((AB +CA) > BC) && ((BC + CA) > AB)) {
+        printf ("\nA, B, C tao thanh tam giac");
+
+        // Kiem tra tam giac ABC co can khong
+        if ((AB == BC) || (AB == CA) || (BC == CA)) {
             printf ("\nABC la tam giac can");
-        } else {
-            printf ("\nABC khong phai la tam giac can");
         }
-        //Tinh chu vi
+        // Tinh chu vi
         chu_vi = AB + BC + CA;
-        printf ("\nChu vi cua tam giac ABC = %.2f", chu_vi);
-        //Tinh dien tich 
+        printf ("\nChu vi tam giac ABC = %.2f", chu_vi);
+        // Tinh dien tich
         p = chu_vi/2;
-        dien_tich = sqrt(p*(p-AB)*(p-BC)*(p-CA));
-        printf ("\nDien tich cua tam giac ABC = %.2f", dien_tich);
-    } else {
-        printf ("ABC Khong tao thanh tam giac");
+        //Dung heron tinh dien tich 
+        dien_tich = sqrt (p*(p-AB)*(p-BC)*(p-CA));
+        printf ("\nDien tich tam giac ABC = %.2f", dien_tich);
+    }
+    // A, B, C khong tao thanh tam giac
+    else {
+        printf ("\nA, B, C khong tao thanh tam giac");
     }
 }
