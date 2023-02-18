785f737145c92dd0a106fc63059121677921141e
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-17 21:28:46 -0500

    Update Baiso020.cpp

--- Baiso020.cpp
+++ Baiso020.cpp
@@ -1,9 +1,22 @@
 #include "stdio.h"
 int main () {
-    /* int i;
+    /* in ra tu 1 -9 ra man hinh 
+    int i;
     for ( i = 0; i<=9 ; i++ ) {
         printf ("\n%d",i);
     } */
+    
+    /* nhap mot so n va in ra tu 1 den so do. Tinh tong cac so da in ra bang vong lap for
+    int n;
+    int total = 0;
+	printf("Nhap n = ");
+	scanf("%d", &n);
+	int i;
+	for(i=0;i<=n;i++){
+		printf("\n%d", i);
+        total = total + i;
+	}
+    printf ("\nTotal = %d", total);*/
     
     int n, i;
     printf ("n = ");
