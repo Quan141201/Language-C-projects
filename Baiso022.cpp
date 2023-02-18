5f3620909ad7732c37dcf978050b4e5b316c208c
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-17 21:36:06 -0500

    Add files via upload

---
+++ Baiso022.cpp
@@ -0,0 +1,26 @@
+#include "stdio.h"
+int main () {
+	/* in so tu 1 - 10 va tinh tong chung 
+	int n = 0;
+	int total = 0;
+	while (n <= 9 ) {
+		n++;
+		printf ("\n n = %d", n);
+		total = n + total;
+	}
+	printf ("\nTotal = %d", total );*/
+
+	// /* nhap mot so n va in ra tu 0 den so do. Tinh tong cac so da in ra bang vong lap while
+	int n;
+	int i = 0;
+	int total = 0;
+	printf ("Nhap n = "); 
+	scanf ("%d", &n);
+
+	while (i <= n) {
+		printf ("\n%d", i);
+		total = total + i;
+		i++;
+	}
+	printf ("\nTotal = %d", total ); 
+}
