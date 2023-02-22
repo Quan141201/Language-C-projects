39363873238223604380ee091f7ca9dd9f2078f9
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-21 23:35:16 -0500

    Add files via upload

---
+++ Baiso024.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h" 
+int main () {
+	int c;
+	
+	do {
+		printf ("\n------MENU-------");
+		printf ("\nNhap vao  0 de thoat ");
+		printf ("\nNhap vao so bat ky khac 0 de tiep tuc ");
+		scanf ("%d",&c);
+	} while (c != 0); 
+}
