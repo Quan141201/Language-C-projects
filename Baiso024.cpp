0d9ad5b512624cd34500c1251457c24b353217bd
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-24 00:39:59 -0500

    Update Baiso024.cpp

--- Baiso024.cpp
+++ Baiso024.cpp
@@ -1,3 +1,9 @@
+// Vong lap do while
+/* Viet chuong trinh hien thi 1 menu sau day len man hinh:
+   - Nhap so 0 de thoat
+   - Nhap so bat ky khac 0 de tiep tuc
+*/
+
 #include "stdio.h" 
 int main () {
 	int c;
