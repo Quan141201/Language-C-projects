67347a8e9da5ebbda7567aa69a061c3cd92e8ba5
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-24 00:12:56 -0500

    Update Baiso009.cpp

--- Baiso009.cpp
+++ Baiso009.cpp
@@ -1,4 +1,5 @@
-// Nhap 2 so nguyen a, b thong qua console. Hay xuat ra man hinh gia tri nho nhat va gia tri lon nhat giua 2 so a,b
+/* Nhap 2 so nguyen a, b thong qua console.
+Hay xuat ra man hinh gia tri nho nhat va gia tri lon nhat giua 2 so a,b */
 
 #include "stdio.h"
 int main () {
