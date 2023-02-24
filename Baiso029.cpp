3fbe6e13af319afe678c8bfab47d78bc633db319
Quan141201 <124326807+Quan141201@users.noreply.github.com>
2023-02-24 01:36:26 -0500

    Add files via upload

---
+++ Baiso029.cpp
@@ -0,0 +1,36 @@
+/* Viet chuong trinh su dung ham de nhap vao so n (n>1),
+xuat ra tat car so chinh phuong nho hon hoac bang n
+*/
+
+/* So chinh phuong x
+ so x = binh phuong khai can cua x 
+*/
+
+#include "stdio.h"
+#include "math.h"
+// tao ham de kt so chinh phuong
+int kiem_tra_SCP (int x) {
+    int kc = sqrt (x);
+    if (pow (kc,2)== x)
+        return 1;
+    else 
+        return 0;
+}
+
+int main () {
+    // Nhap so den khi n > 1
+    int n;
+    do {
+    printf ("Nhap n = ");
+    scanf ("%d", &n);
+    } while (n<=1);
+
+    // Goi ham de tim ra tat ca so chinh phuong <=n
+    printf ("Cac so chinh phuong nho hon hoac bang n la: ");
+    for (int i=2; i<=n; i++) {
+        int cp = kiem_tra_SCP (i);
+        if (cp == 1) {
+            printf ("\n%d",i );
+        }
+    }
+}
