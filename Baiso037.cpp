c0bd04c9b709339820f9abcf41ba779b207a998c
Minh Quan Nguyen <124326807+Quan141201@users.noreply.github.com>
2023-03-17 09:32:01 -0400

    Update Baiso037.cpp

--- Baiso037.cpp
+++ Baiso037.cpp
@@ -1,49 +1,51 @@
 /* Nhap vao mang 1 chieu gom n phan tu:
 Tinh trung binh cong cac phan tu trong mang;
-Tinh trung binhf cong cac so duong trong mang;
+Tinh trung binh cong cac so duong trong mang;
 */
 
 #include "stdio.h"
-float trung_binh_cong (int y [], int n) {
+// Ham tinh trung binh cong 
+float trung_binh_cong (int x[], int n) {
 	int tong = 0;
 	for (int i = 0; i < n; i++) {
-		tong += y[i];
+		tong += x[i];
 	}
-	return (float)tong/n; 
+	return (float) tong/n; 
 }
 
-float trung_binh_cong_so_duong (int y [], int n) {
+// Ham tinh trung binh cong so duong cua so duong 
+float trung_binh_cong_so_duong (int x[], int n) {
 	int tong = 0;
-	int so_luong_so_duong = 0;
+	int so_duong = 0;
 	for (int i = 0; i < n; i++) {
-		if (y[i] >= 0) {
-			tong += y[i];
-			so_luong_so_duong ++;
+		if (x[i] >= 0) {
+			tong += x[i];
+			so_duong ++;
 		}
 	}
-	return so_luong_so_duong > 0 ? (float)tong/so_luong_so_duong : -1;
+	return so_duong > 0? (float) tong/ so_duong : -1; 
 }
 
 int main () {
-	int a [100];
-	int n; 
+	// Khai bao so phan tu n + mang 
+	int a[100], n;
 	
+	// Nhap so phan tu 1 - 100
 	do {
-		printf ("Nhap vao so phan tu n ( n < 0 <= 100) cua mang: ");
+		printf ("Nhap vao so phan tu n (0 < n <= 100) cua mang: ");
 		scanf ("%d", &n);
-	} while ( n < 1 || n >= 100);
+	} while (n < 1 || n > 100);
 	
+	// Nhap mang 
 	for (int i = 0; i < n; i++) {
-		printf ("\na [%d] = ", i);
+		printf ("a[%d] = ", i);
 		scanf ("%d", &a[i]);
 	}
 	
-	printf ("Trung binh cong cua mang = %.2f", trung_binh_cong(a, n));
+	// Goi ham tinh trung binh cong
+	printf ("Trung binh cong cua ham = %.2f", trung_binh_cong (a, n));
 	
-	float tbcsd = trung_binh_cong_so_duong (a,n);
-	if (tbcsd > 0) {
-		printf ("\nTrung binh cong so duong cua mang = %.2f", tbcsd);
-	} else {
-		printf ("\nMang khong co so duong!");
-	}
+	// Goi ham tinh trung binh cong so duong 
+	float tbcsd = trung_binh_cong_so_duong (a, n);
+	printf ("\nTrung binh cong so duong cua ham = %.2f", tbcsd);
 }
